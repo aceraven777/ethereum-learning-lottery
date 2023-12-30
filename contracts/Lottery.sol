74c1fb94d3ea018e8b0b3dfb2efd88d434461c32
Aris Lacdao <aceraven777@gmail.com>
2023-12-30 17:54:56 +0800

    Episode 73 - Entering the Lottery

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -4,8 +4,13 @@ pragma solidity ^0.8.9;
 
 contract Lottery {
     address public manager;
+    address[] public players;
 
     constructor() {
         manager = msg.sender;
+    }
+
+    function enter() external payable {
+        players.push(msg.sender);
     }
 }
